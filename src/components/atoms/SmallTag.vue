16676554205763f5ff902ed70e8f74a5617ca6b2
Dawntraoz <albasilventefu@gmail.com>
2022-01-25 22:24:07 +0100

    feat: add address icon and small tag atom

---
+++ src/components/atoms/SmallTag.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="bg-violet-100 dark:bg-violet-700 dark:text-red-200 py-[3px] px-2 rounded-md">
+    <slot></slot>
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SmallTag'
+});
+</script>
